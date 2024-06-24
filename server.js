be8ca27962c020b65f948869f3f2bc7b64fd0ab9
11111Divyam11111 <126981502+11111Divyam11111@users.noreply.github.com>
2024-06-24 16:55:44 +0530

    url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: `${process.env.SNOWTAIL_URL}`,
+    origin: [`${process.env.SNOWTAIL_URL}`, `${process.env.BASE_URL}`]
   })
 );
 app.use(bodyParser.json());
